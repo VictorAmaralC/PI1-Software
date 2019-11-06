042a6c595a9b6211c2e8773fea14e4ca2e17be19
Iago Theophilo <iagotheophilolim@gmail.com>
2019-11-06 13:59:37 -0300

    #1 Base do projeto Co-authored-by: Amaral <victorchubaka@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PI1-Software
 Repositório de atividades relacionadas a matéria de Projeto integrador para engenharias 1 da UnB
+# Bootstrap:
+    https://getbootstrap.com.br/docs/4.1/getting-started/introduction/
+# Executando PHP:
+    Versão: PHP 7.2.24
+    execute o comando no terminal:
+        php -S localhost:8000
+    Vai para o browser da sua escolha e pesquise por:
+        localhost:8000
---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="imagens/logo.png">
+
+    <title>Template de login, usando Bootstrap.</title>
+
+    <!-- Principal CSS do Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Estilos customizados para esse template -->
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin" action="view/home.php">
+      <img class="mb-4" src="imagens/logo.png" alt="" width="220" height="220">
+      <h1 class="h3 mb-3 font-weight-normal">Faça login Professor</h1>
+      <label for="inputEmail" class="sr-only">Endereço de email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Seu email" required autofocus>
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Lembrar de mim
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2019</p>
+    </form>
+  </body>
+</html>
