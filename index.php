26d1812994400aad9ffe12ee81ab5b68d6aa65b7
Aline Lermen <aline.helena.lermen@gmail.com>
2019-12-01 14:23:19 -0300

    #46 Ajusta detalhes da página de login e adiciona o termo nela

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="author" content="">
     <link rel="icon" href="imagens/logo.png">
 
-    <title>Template de login, usando Bootstrap.</title>
+    <title>BioChamada</title>
 
     <!-- Principal CSS do Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -19,7 +19,7 @@
   <body class="text-center">
     <form class="form-signin" action="view/home.php">
       <img class="mb-4" src="imagens/logo.png" alt="" width="220" height="220">
-      <h1 class="h3 mb-3 font-weight-normal">Faça login Professor</h1>
+<!--      <h1 class="h3 mb-3 font-weight-normal">Faça login Professor</h1> -->
       <label for="inputEmail" class="sr-only">Endereço de email</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Seu email" required autofocus>
       <label for="inputPassword" class="sr-only">Senha</label>
@@ -29,7 +29,11 @@
           <input type="checkbox" value="remember-me"> Lembrar de mim
         </label>
       </div>
+      <a href="termo.php" class="btn btn-link btn-sm">
+        Esteja ciente do termo para utilização do website. Leia aqui!
+      </a>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+      
       <p class="mt-5 mb-3 text-muted">&copy; 2019</p>
     </form>
   </body>
---
+++ termo.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="imagens/logo.png">
+
+    <title>BioChamada</title>
+
+    <!-- Principal CSS do Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+
+  <body style="background-color:#f5f5f5; margin-top:30px; margin-left:50px; margin-right:50px; margin-bottom:50px;">
+    <h2 style="text-align:center;">TERMO DE CONFIDENCIALIDADE E NÃO-DIVULGAÇÃO</h2>
+    <br />
+    <p style="text-align:justify;">Ao utilizar a aplicação <i>web</i> oferecida pela BioChamada, o 
+        usuário (professor(a) ou instituição) assume o compromisso de manter confidencialidade de toda 
+        informação referente aos alunos e/ou obtida no controle de frequência realizado nas aulas.</p>
+    <p style="text-align:justify;">O usuário se compromete a:</p>
+    <ol>
+        <li>não repassar o conhecimento das informações pessoais dos alunos, responsabilizando-se 
+            por todas as pessoas que vierem a ter acesso às informações, por seu intermédio.</li>
+    </ol>
+
+    <p style="text-align:justify;">A exposição dos dados/informações somente poderá ocorrer 
+        nas seguintes situações:</p>
+    <ol>
+        <li>quando o titular ou seu responsável legal consentir, de forma específica e destacada, 
+            para finalidades específicas;</li>
+        <li>sem fornecimento do consentimento do titular, nas hipóteses em que for indispensável para:
+            <ol type="a">
+                <li>cumprimento de obrigação legal;</li>
+                <li>garantia da prevenção à fraude e da segurança do titular.</li> 
+            </ol>
+        </li>
+    </ol>
+    <p style="text-align:justify;">O tratamento de dados pessoais de crianças e adolescentes 
+        deverá ser realizado com o consentimento específico e em destaque dado por pelo menos 
+        um dos pais ou pelo responsável legal.</p>
+
+    <br />
+    <div style="text-align:center;">
+        <a href="index.php" class="btn btn-primary btn-lg">Entendi</a>
+    </div>
+  </body>
+
+</html>
